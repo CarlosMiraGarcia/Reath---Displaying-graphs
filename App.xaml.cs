3f3f5fff78e8abda8bbba83a75346f47a6065d51
CarlosMiraGarcia <carlosgarciamira@gmail.com>
2021-03-08 11:19:54 +0000

    Adding repository

--- App.xaml.cs
+++ App.xaml.cs
@@ -12,6 +12,14 @@ namespace ReathUIv0._1
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
-    {
+    {        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            Window window = new Graphs();
+            Graphs context = new Graphs();
+            window.DataContext = context;
+            window.Show();
+        }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,6 +20,7 @@ namespace ReathUIv0._1
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
